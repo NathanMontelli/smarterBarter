39cc137d537345c0711adc05631a8adce359cc00
Nathan Montelli <nathanmontelli@gmail.com>
2022-01-04 10:22:39 -0800

    commas

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -17,10 +17,10 @@ router.get('/items', passport.authenticate('jwt'), async function (req, res) {
 // POST one post
 router.post('/items', passport.authenticate('jwt'), async function (req, res) {
   const item = await Item.create({
-    item_name: req.body.item_name
-    title: req.body.title
-    body: req.body.body
-    cid: req.body.cid
+    item_name: req.body.item_name,
+    title: req.body.title,
+    body: req.body.body,
+    cid: req.body.cid,
     uid: user.id
   })
   res.json()
