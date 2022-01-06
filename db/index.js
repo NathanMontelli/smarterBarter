9efdc1b950f02fee7ab4f6e3cec8339a3501783b
Harrison Reich <hreich1998@gmail.com>
2022-01-05 20:06:13 -0800

    updated connection password

--- db/index.js
+++ db/index.js
@@ -1,3 +1,3 @@
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize(process.env.NODE_ENV === 'production' ? process.env.JAWSDB_URL : 'mysql://root:Abc123!!@localhost:3306/smarterBarter_db')
+module.exports = new Sequelize(process.env.NODE_ENV === 'production' ? process.env.JAWSDB_URL : 'mysql://root:rootroot@localhost:3306/smarterBarter_db')
