3bd82f9999b7317900ab02a4f0bdcdffecdbcdee
LohasOT <oliverhoang93@gmail.com>
2022-01-05 22:23:45 -0800

    code

--- db/index.js
+++ db/index.js
@@ -1,3 +1,7 @@
+require('dotenv').config()
+
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize(process.env.NODE_ENV === 'production' ? process.env.JAWSDB_URL : 'mysql://root:Abc123!!@localhost:3306/smarterBarter_db')
+const sequelize = new Sequelize(process.env.LOCAL_DB_URL)
+
+module.exports = sequelize
