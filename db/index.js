da18aad7bcad584d881b4e8559ed81f0a43206b3
Harrison Reich <hreich1998@gmail.com>
2022-01-12 11:00:40 -0800

    code

--- db/index.js
+++ db/index.js
@@ -2,4 +2,4 @@ require('dotenv').config()
 
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize(process.env.JAWSDB_URL || process.env.LOCALDB_URL)
+module.exports = new Sequelize(process.env.JAWSDB_URL || process.env.LOCAL_DB_URL)
